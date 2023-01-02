5812bea42bef9869126998b019a6608e6357a0e8
Mithun <tkmithun@gmail.com>
2023-01-02 18:50:04 +0530

    initial commit

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return {
+        "message": "success"
+    }
 
 
 if __name__ == '__main__':
-    app.run(debug=True)    
+    app.run(debug=True, port=5000)    
